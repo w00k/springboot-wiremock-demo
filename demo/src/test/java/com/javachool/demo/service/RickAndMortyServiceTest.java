94b51f5f7914fbc926223cede1548df6fdeb615b
Francisco Arenas <franciscoarenas.globant@latam.com>
2024-05-06 13:03:39 -0400

    more test

--- demo/src/test/java/com/javachool/demo/service/RickAndMortyServiceTest.java
+++ demo/src/test/java/com/javachool/demo/service/RickAndMortyServiceTest.java
@@ -1,5 +1,6 @@
 package com.javachool.demo.service;
 
+import com.javachool.demo.exception.CommunicationException;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,8 @@ import org.springframework.beans.factory.annotation.Value;
 import static com.javachool.demo.utils.UtilTransformFile.objectFromJson;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 
@@ -51,4 +54,37 @@ public class RickAndMortyServiceTest {
     assertEquals(characterResponse.getName(), response.getName());
     assertEquals(locationResponse.getDimension(), response.getOrigin().getDimension());
   }
+
+    @Test
+    void getCharacterByIdWhenCharacterNotFoundTest() throws IOException {
+        //prepate data
+        int idCharacter = 1001;
+        CharacterResponse characterResponse = objectFromJson("/stubs/response_404_character_1001.json", CharacterResponse.class);
+
+        //mock
+        when(restTemplate.getForObject(characterURL + idCharacter, CharacterResponse.class)).thenReturn(characterResponse);
+
+        //verify
+        assertThrows(CommunicationException.class, ()-> rickAndMortyService.callAclService(idCharacter));
+    }
+
+    @Test
+    void getCharacterByIdWhenLocationNotFoundTest() throws IOException {
+        //prepate data
+        int idCharacter = 1001;
+        CharacterResponse characterResponse = objectFromJson("/stubs/response_200_character_1.json", CharacterResponse.class);
+        LocationResponse locationResponse = objectFromJson("/stubs/response_404_location_1001.json", LocationResponse.class);
+        String locationURL = "https://rickandmortyapi.com/api/location/3";
+        //mock
+        when(restTemplate.getForObject(characterURL + idCharacter, CharacterResponse.class)).thenReturn(characterResponse);
+        when(restTemplate.getForObject(locationURL , LocationResponse.class)).thenReturn(locationResponse);
+
+        //call service
+        RickAndMortyResponse response = rickAndMortyService.callAclService(idCharacter);
+
+        //verify
+        assertNotNull(response);
+        assertEquals(characterResponse.getName(), response.getName());
+        assertNull(response.getOrigin().getDimension());
+    }
 }
