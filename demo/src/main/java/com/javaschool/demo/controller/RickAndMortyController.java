f309853bfb0f6218579e7970bcde404759709897
Francisco Arenas <franciscoarenas.globant@latam.com>
2024-05-08 23:00:09 -0400

    change @Controller for @RestController, for swagger

--- demo/src/main/java/com/javaschool/demo/controller/RickAndMortyController.java
+++ demo/src/main/java/com/javaschool/demo/controller/RickAndMortyController.java
@@ -3,14 +3,15 @@ package com.javaschool.demo.controller;
 import com.javaschool.demo.config.LoggerConfiguration;
 import com.javaschool.demo.model.RickAndMortyResponse;
 import com.javaschool.demo.service.RickAndMortyService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RickAndMortyController {
 
     private final LoggerConfiguration logger = LoggerConfiguration.getLogger("RickAndMortyController");
